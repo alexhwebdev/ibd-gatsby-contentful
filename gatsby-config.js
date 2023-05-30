68fc580226ef42f71301565e922a757fd39bd974
alexhwebdev@gmail.com <alexhwebdev@gmail.com>
2023-05-30 15:31:03 -0700

    TESTING gatsby-plugin-gatsby-cloud Redirect 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,7 +95,7 @@ module.exports = {
       },
     },
 
-    `gatsby-plugin-client-side-redirect` // keep it in last in list
+    // `gatsby-plugin-client-side-redirect` // keep it in last in list
   ],
 
   flags: {
